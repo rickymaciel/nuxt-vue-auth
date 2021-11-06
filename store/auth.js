2e245f8c13968a3a87e299094d14e527e8003cd0
rickymaciel <rmacielb3@gmail.com>
2021-11-06 18:41:43 -0300

    update auth

--- store/auth.js
+++ store/auth.js
@@ -1,58 +1,52 @@
+export const key_cookie = 'pinbusStore';
+
 export const state = () => ({
   token: "",
   isLoggedIn: false,
-})
+});
 
 export const mutations = {
   SET_TOKEN(state, params) {
-    state.token = params.token ?? state.token
-    state.isLoggedIn = !!params.token;
+    state.token = params.token ?? state.token;
+    state.isLoggedIn = !!state.token;
   },
   SET_LOGOUT(state) {
     state.token = "";
     state.isLoggedIn = false;
   },
-}
+};
 
 export const actions = {
   async getToken(context) {
     try {
-
       const response = await this.$axios.get(`${process.env.VUE_APP_API_URL}/auth/token`);
 
-      const {
-        token
-      } = response.data.content
+      const { token } = response.data.content;
 
-      //this.$cookies.set('pinbusStore', {token, isLoggedIn: !!token }, { domain: process.env.VUE_APP_COOKIE_DOMAIN })
+      this.$cookies.set( key_cookie, { token }, { domain: process.env.VUE_APP_COOKIE_DOMAIN } );
 
-      this.$cookies.set('pinbusStore', token);
-
-      context.commit('SET_TOKEN', { token });
+      context.commit("SET_TOKEN", { token });
 
     } catch (error) {
-      console.error('getToken', {
-        error
+      console.error("getToken", {
+        error,
       });
     }
   },
   async getLogout(context) {
     try {
-
-      context.commit('SET_LOGOUT');
-
-      this.$cookies.remove('pinbusStore');
-
+      this.$cookies.remove(key_cookie);
+      context.commit("SET_LOGOUT");
     } catch (error) {
-      console.error('getLogout', {
-        error
-      })
+      console.error("getLogout", {
+        error,
+      });
     }
   },
-}
+};
 
 export const getters = {
   token: (state) => {
-    return state.token
+    return state.token;
   },
-}
+};
